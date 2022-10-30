45e33918ee1d1dc00d40ae28d8ee20094fde1e49
christinejoyestacion <116147896+christinejoyestacion@users.noreply.github.com>
2022-10-30 22:19:05 +0800

    Update Bicycle.Java.java

--- Bicycle.Java.java
+++ Bicycle.Java.java
@@ -1,3 +1,21 @@
 public class Bicycle.Java
 {
+private String ownerName;
+private String tagNo;
+
+public Bicycle() {
+ownerName = "Unknown";
+tagNo = "Unkown";
+}
+public String getOwnerName() {
+return ownerName;
+}
+public void setOwnerName(String name){
+ownerName = name;
+}
+public void settagNo(String num){
+tagNo = num;
+}
+public  String gettagNo(){
+return tagNo;
 }
